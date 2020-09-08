7a97a04572b71d67294ca06ac88006565708b476
marco corapi <marco.corapi@outlook.it>
2020-09-08 22:05:26 +0200

    updated view for index

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -38,8 +38,10 @@ class MealsController < ApplicationController
     end
   end
 
-  def destroy
-    
+  def destroy 
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+    redirect_to meals_path
   end
 
   private
