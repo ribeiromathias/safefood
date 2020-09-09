27b7dd3f8eba96dffa4d3be39cb3a6f0cb3a4ecd
marco corapi <marco.corapi@outlook.it>
2020-09-09 16:43:34 +0200

    seeds are now working

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,21 +38,21 @@ puts "user created... meals for sellers..."
 file = URI.open('https://i.ytimg.com/vi/xCc2Qn4vxt4/maxresdefault.jpg')
 celiac_focaccia = Meal.new
 celiac_focaccia.user = matheus
-celiac_focaccia.name
-celiac_focaccia.offer_price
-celiac_focaccia.description
-celiac_focaccia.restriction
+celiac_focaccia.name = "Gluten free focaccia"
+celiac_focaccia.offer_price = 300
+celiac_focaccia.description = "This gluten free rosemary focaccia is made entirely gluten free"
+celiac_focaccia.restriction = "Gluten-free"
 celiac_focaccia.photo.attach(io: file, filename: 'nes.png')
 celiac_focaccia.save!
 
 puts "user created... meals for buyer..."
 
 file = URI.open('https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/dark-chocolate-1296x728-header.jpg?w=1155&h=1528')
-chocolate_for_diabetics = Meal.new
-chocolate_for_diabetics.user = marcel
-chocolate_for_diabetics.name
-chocolate_for_diabetics.offer_price
-chocolate_for_diabetics.description
-chocolate_for_diabetics.restriction
-chocolate_for_diabetics.photo.attach(io: file, filename: 'nes.png')
-chocolate_for_diabetics.save!
+chocolate_for_vegan = Meal.new
+chocolate_for_vegan.user = marcel
+chocolate_for_vegan.name = "Amazing vegan chocolate"
+chocolate_for_vegan.offer_price = 200
+chocolate_for_vegan.description = "These chocolate bars are made with vegan ingredients: cacao, coconut milk, maple syrup and salt."
+chocolate_for_vegan.restriction = "Vegan"
+chocolate_for_vegan.photo.attach(io: file, filename: 'nes.png')
+chocolate_for_vegan.save!
