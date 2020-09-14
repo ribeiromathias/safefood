8aaa918575220b36b0901240a63e4dffb7e9a066
uros simcic <inbox.si@gmail.com>
2020-09-14 15:13:17 -0300

    refferences were fixed

--- db/migrate/20200914144413_create_reviews.rb
+++ db/migrate/20200914144413_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.text :content
-      t.references :meals, null: false, foreign_key: true
+      t.references :meal, null: false, foreign_key: true
 
       t.timestamps
     end
