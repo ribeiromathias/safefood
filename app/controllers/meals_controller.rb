f4e19c9caf87cc517f81ecedc00b25d0a0961ae5
uros simcic <inbox.si@gmail.com>
2020-09-15 12:00:33 -0300

    review and login fixes

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,5 @@
 class MealsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :index]
+  skip_before_action :authenticate_user!, only: [ :show, :index ]
   def new
     @meal = Meal.new
     authorize @meal
