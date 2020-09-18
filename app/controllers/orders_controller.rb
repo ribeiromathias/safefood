b019519c3f8b28b0247515d61d2d7c73a3543b94
Mathias Ribeiro <mathiaswrc@gmail.com>
2020-09-18 15:46:19 -0300

    done pending order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,11 +16,21 @@ class OrdersController < ApplicationController
     authorize @order
     if ["pending", "cancelled", "confirmed"].include?(params[:status])
       @order.update(status: params[:status])
-      redirect_to restaurant_path(id: @order.order_items.last.meal.user.id, restriction: @order.order_items.last.meal.restriction)
+      if params[:status] == "pending"
+        redirect_to pending_path 
+      elsif params[:status] == "cancelled"
+        redirect_to restaurant_path(id: @order.order_items.last.meal.user.id, restriction: @order.order_items.last.meal.restriction)
+        
+      end
     else
       redirect_to orders_path, alert: "Something went wrong"
     end
 
+  end
+
+  def pending_orders
+    @pending_orders = current_user.orders.where(status: "pending")
+    authorize(Order) 
   end
 
   private
